c25dc8addae245047de247fc235eb1835eae5987
20cameronw <89608181+20cameronw@users.noreply.github.com>
2022-11-02 01:47:42 -0500

    Added behavior for enemy states, imported models, and added drill to shop

--- SpheerURP/Assets/_Scripts/Monobehaviors/EnemyScripts/EnemyApproachingState.cs
+++ SpheerURP/Assets/_Scripts/Monobehaviors/EnemyScripts/EnemyApproachingState.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class EnemyApproachingState : EnemyState
 {
-    public EnemyAttackState enemyAttackState;
-    public bool hasArrived;
+    [SerializeField] private EnemyAttackState enemyAttackState;
+    [SerializeField] private Transform attackPoint;
+    [SerializeField] private float speed;
+    private bool hasArrived;
+    private bool cr_running;
+    private bool approaching;
+
 
     public override EnemyState RunState()
     {
+        approaching = true;
+
         if (hasArrived)
         {
+            approaching = false;
+            hasArrived = false;
             return enemyAttackState;
         }
         return this;
+    }
+
+    void Update()
+    {
+        if (approaching)
+        {
+            var step =  speed * Time.deltaTime; 
+            transform.parent.parent.position = Vector3.MoveTowards(transform.parent.parent.position, attackPoint.position, step);
+        }
+
+        if (Vector3.Distance(transform.parent.parent.position, attackPoint.position) < 0.01f)
+        {
+            hasArrived = true;
+        }
+        else
+        {
+            hasArrived = false;
+        }
     }
 }
